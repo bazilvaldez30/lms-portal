99250e9166ce4267aa0244b1d929d20789ad4984
John Bazil Milan Valdez <bazilvaldez30@gmail.com>
2024-04-20 15:15:55 +0800

    Setup react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter, Montserrat } from 'next/font/google'
-import './styles/globals.css'
-import Navbar from './components/navbar'
+import type { Metadata } from "next";
+import { Inter, Montserrat } from "next/font/google";
+import "./styles/globals.css";
+import Navbar from "./components/navbar";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const inter = Inter({ subsets: ['latin'] })
-const mont = Montserrat({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+const mont = Montserrat({ subsets: ["latin"] });
+const queryClient = new QueryClient();
 
 export const metadata: Metadata = {
-  title: 'OLAS',
-  description: 'Learning Management System',
-}
+  title: "OLAS",
+  description: "Learning Management System",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={mont.className}>
-        <Navbar />
-        {children}
-      </body>
+    <html lang="en">
+      <QueryClientProvider client={queryClient}>
+        <body className={mont.className}>
+          <Navbar />
+          {children}
+        </body>
+      </QueryClientProvider>
     </html>
-  )
+  );
 }
