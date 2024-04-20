e7d227ee22219d94492912bee3c3d9fe305dce5f
John Bazil Milan Valdez <bazilvaldez30@gmail.com>
2024-04-20 16:22:40 +0800

    Remove react query dev tools

--- src/app/shared/Util/providers.tsx
+++ src/app/shared/Util/providers.tsx
@@ -1,13 +1,11 @@
-import ReactNode, { useState } from "react";
+"use client";
+
+import React, { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Provider({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
