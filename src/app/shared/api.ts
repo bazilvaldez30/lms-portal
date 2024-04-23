2ed0b1a0e91af9cfa8af09946099ee92de273731
John Bazil Milan Valdez <bazilvaldez30@gmail.com>
2024-04-23 18:49:15 +0800

    fix api middleware error

--- src/app/shared/api.ts
+++ src/app/shared/api.ts
@@ -21,56 +21,11 @@ const setAccessToken = (
 };
 
 // Define the handleRefresh function to handle token refresh logic
-const handleRefresh = async (
+/* const handleRefresh = async (
   error: AxiosError,
   refreshAttempts: number,
   router: any,
-): Promise<AxiosResponse<any> | never> => {
-  if (error.response) {
-    const originalRequest = error.config as CustomAxiosRequestConfig; // Cast error.config to CustomAxiosRequestConfig
-    if (
-      error.response.status === 401 &&
-      originalRequest &&
-      !originalRequest._retry &&
-      refreshAttempts < 3
-    ) {
-      originalRequest._retry = true;
-
-      try {
-        const refresh = Cookies.get("refresh");
-        if (!refresh) {
-          const currentPath = router.asPath;
-          Cookies.set("return_path", currentPath);
-          return Promise.reject(error.response); // Reject with the original error
-        }
-
-        const refreshResponse = await api.post("auth/token/refresh/", {
-          refresh,
-        });
-
-        Cookies.set("access", refreshResponse.data.access);
-
-        // Ensure that originalRequest.headers is defined before accessing its properties
-        if (originalRequest.headers) {
-          originalRequest.headers.Authorization = `Bearer ${refreshResponse.data.access}`;
-        }
-
-        return axios(originalRequest);
-      } catch (refreshError) {
-        console.error("Token refresh failed:", refreshError);
-        return Promise.reject(error.response); // Reject with the original error
-      }
-    } else if (refreshAttempts >= 3) {
-      console.error("Token refresh attempts exceeded. Redirecting to login.");
-      Cookies.remove("access");
-      Cookies.remove("refresh");
-      Cookies.remove("user");
-      return Promise.reject(error.response); // Reject with the original error
-    }
-  }
-  // Return the error response for both failed requests and bad requests
-  return error.response as AxiosResponse<any>;
-};
+): Promise<AxiosResponse<any> | never> => {};
 
 api.interceptors.request.use(
   (config) => {
@@ -83,13 +38,57 @@ api.interceptors.request.use(
   (error) => {
     return Promise.reject(error);
   },
-);
+); */
+
+let refreshAttempts = 0; // Initialize the counter
 
 api.interceptors.response.use(
   (response) => response, // Return the response for all successful requests
   async (error) => {
-    const router = useRouter();
-    return handleRefresh(error, 0, router); // Start with 0 refresh attempts
+    if (error.response) {
+      const originalRequest = error.config as CustomAxiosRequestConfig; // Cast error.config to CustomAxiosRequestConfig
+      if (
+        error.response.status === 401 &&
+        originalRequest &&
+        !originalRequest._retry &&
+        refreshAttempts < 3
+      ) {
+        originalRequest._retry = true;
+
+        try {
+          const refresh = Cookies.get("refresh");
+          if (!refresh) {
+            const currentPath = useRouter().asPath;
+            Cookies.set("return_path", currentPath);
+            return Promise.reject(error.response); // Reject with the original error
+          }
+
+          const refreshResponse = await api.post("auth/token/refresh/", {
+            refresh,
+          });
+
+          Cookies.set("access", refreshResponse.data.access);
+
+          // Ensure that originalRequest.headers is defined before accessing its properties
+          if (originalRequest.headers) {
+            originalRequest.headers.Authorization = `Bearer ${refreshResponse.data.access}`;
+          }
+
+          return axios(originalRequest);
+        } catch (refreshError) {
+          console.error("Token refresh failed:", refreshError);
+          return Promise.reject(error.response); // Reject with the original error
+        }
+      } else if (refreshAttempts >= 3) {
+        console.error("Token refresh attempts exceeded. Redirecting to login.");
+        Cookies.remove("access");
+        Cookies.remove("refresh");
+        Cookies.remove("user");
+        return Promise.reject(error.response); // Reject with the original error
+      }
+    }
+    // Return the error response for both failed requests and bad requests
+    return error.response as AxiosResponse<any>;
   },
 );
 
