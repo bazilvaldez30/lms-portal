aafa88062351ca7289a8613fb596f49ba3fafcc2
Kayl Jeremiah Teves <kylhrmys@gmail.com>
2024-04-28 15:58:26 +0800

    enrollment UI

--- src/app/dashboard/enrollment/enrollment.tsx
+++ src/app/dashboard/enrollment/enrollment.tsx
@@ -2,86 +2,65 @@ import React from "react";
 
 export default function Enrollment() {
   return (
-    <div className="">
-      <div className="mb-4 grid grid-cols-12 gap-4">
-        <div className="col-span-8 flex h-28 items-center justify-center rounded-md bg-gray-50 dark:bg-gray-800">
-          <p className="text-2xl text-gray-400 dark:text-gray-500">
-            <svg
-              className="h-3.5 w-3.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 18"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 1v16M1 9h16"
-              />
-            </svg>
-          </p>
-        </div>
-        <div className="col-span-4 flex h-28 items-center justify-center rounded-md bg-gray-50 dark:bg-gray-800">
-          <p className="text-2xl text-gray-400 dark:text-gray-500">
-            <svg
-              className="h-3.5 w-3.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 18"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 1v16M1 9h16"
-              />
-            </svg>
-          </p>
-        </div>
+    <div className=" flex grid auto-rows-[150px] gap-5 lg:grid-cols-12 ">
+      <div className="order-0 row-span-1 flex  flex-col rounded-xl border border-solid px-5 py-4 lg:col-span-4">
+        <p className=" mb-4 text-lg font-bold text-gray-500">Programs</p>
+        <form action="">
+          <label
+            htmlFor=""
+            className="mb-3 block text-sm font-semibold text-black"
+          >
+            Choose program
+          </label>
+          <select className="form-input">
+            <option value="">Select Program</option>
+          </select>
+        </form>
       </div>
-
-      <div className="mb-4 grid grid-cols-12 gap-4">
-        <div className="col-span-4 flex h-28 items-center justify-center rounded-md bg-gray-50 dark:bg-gray-800">
-          <p className="text-2xl text-gray-400 dark:text-gray-500">
-            <svg
-              className="h-3.5 w-3.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 18"
+      <div className=" order-3 row-span-4 rounded-xl border border-solid px-5 py-4 lg:order-2 lg:col-span-8">
+        <p className="mb-4 text-lg font-bold text-gray-500">How to enroll</p>
+      </div>
+      <div className=" order-2 row-span-3 rounded-xl border border-solid px-5 py-4 lg:order-3 lg:col-span-4">
+        <p className="mb-4 text-lg font-bold text-gray-500">
+          Upload Requirements
+        </p>
+        <div className="flex flex-col gap-5">
+          <form action="" className="flex flex-col">
+            <label
+              htmlFor=""
+              className="mb-3 block text-sm font-semibold text-black "
             >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 1v16M1 9h16"
-              />
-            </svg>
-          </p>
-        </div>
-        <div className="col-span-8 flex h-28 items-center justify-center rounded-md bg-gray-50 dark:bg-gray-800">
-          <p className="text-2xl text-gray-400 dark:text-gray-500">
-            <svg
-              className="h-3.5 w-3.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 18"
+              Birthcertificate
+            </label>
+            <input type="file" className="form-input" />
+            <button className="button-primary mt-3 rounded-lg bg-red-800 md:text-[10px]">
+              UPLOAD
+            </button>
+          </form>
+          <form action="" className="flex flex-col">
+            <label
+              htmlFor=""
+              className="mb-3 block text-sm font-semibold text-black"
             >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 1v16M1 9h16"
-              />
-            </svg>
-          </p>
+              Form-137
+            </label>
+            <input type="file" className="form-input" />
+            <button className="button-primary mt-3 rounded-lg bg-red-800 md:text-[10px]">
+              UPLOAD
+            </button>
+          </form>
+          <form action="" className="flex flex-col">
+            <label
+              htmlFor=""
+              className="mb-3 block text-sm font-semibold text-black"
+            >
+              ID/Picture
+            </label>
+            <input type="file" className="form-input" />
+            <button className="button-primary mt-3 rounded-lg bg-red-800 md:text-[10px]">
+              UPLOAD
+            </button>
+          </form>
         </div>
       </div>
     </div>
